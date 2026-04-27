2e5d47c188086d31caf1a743c5c3404ba8d9435b
Nighty3098 <night3098games@gmail.com>
2026-04-27 16:00:09 +0700

    Fix

--- create_user.sql
+++ create_user.sql
@@ -1,12 +1,12 @@
 DO $$
 BEGIN
-    IF NOT EXISTS (SELECT FROM pg_catalog.pg_roles WHERE rolname = 'kwork_user') THEN
-        CREATE USER kwork_user WITH PASSWORD 'password';
-        GRANT ALL PRIVILEGES ON DATABASE kwork_bot TO kwork_user;
-        ALTER USER kwork_user WITH SUPERUSER;
+    IF NOT EXISTS (SELECT FROM pg_catalog.pg_roles WHERE rolname = 'admin') THEN
+        CREATE USER admin WITH PASSWORD 'password';
+        GRANT ALL PRIVILEGES ON DATABASE kwork TO admin;
+        ALTER USER admin WITH SUPERUSER;
     END IF;
 END
 $$;
 
-GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA public TO kwork_user;
-GRANT ALL PRIVILEGES ON ALL SEQUENCES IN SCHEMA public TO kwork_user;
+GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA public TO admin;
+GRANT ALL PRIVILEGES ON ALL SEQUENCES IN SCHEMA public TO admin;
